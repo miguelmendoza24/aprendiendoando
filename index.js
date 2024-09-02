e430f1ff918ed98d5020704fa594132874b361cf
miguel <miguelevamendo@gmail.com>
2024-09-02 07:44:25 -0600

    practica js

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("hola munndo");
+const arregloNumeros = [1, 2, 3, 4, 5]
+console.log(arregloNumeros[4]);
+
+function mayorEdad (edad) {
+  if (edad >= 18) {
+    console.log("eres mayor de edad");
+  } else {
+    console.log("no eres mayor de edad");
+  }
+
+}
+mayorEdad(18);
