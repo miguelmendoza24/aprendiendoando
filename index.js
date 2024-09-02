3472385d47db14294d48b7ef6922a6c242da34e9
miguel <miguelevamendo@gmail.com>
2024-09-02 08:04:47 -0600

    for añadido

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ console.log("hola munndo");
 const arregloNumeros = [1, 2, 3, 4, 5]
 console.log(arregloNumeros[4]);
 
+for (let index = 0;  index< arregloNumeros.length; index++) {
+  const element = arregloNumeros[index];
+  console.log(element)
+}
+
+
 function mayorEdad (edad) {
   if (edad >= 18) {
     console.log("eres mayor de edad");
